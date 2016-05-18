9ea7b88c708974ab23383a2338f2a9a478583be3
Meiramgul <meiramgul.gubaidullina@ce.sdu.edu.kz>
2016-05-18 16:40:49 +0600

    Create bio.php

---
+++ 1A04/GubaidullinaMeiramgul/bio.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+    <script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="action.js"></script>
+
+<link rel="stylesheet" type="text/css" href="style.css" />
+<?php
+include("mainpage.php");
+?>
+</head>
+<body>
+<br/>
+    <br/>
+    <br/>
+    <br/>
+<div id="panel5">
+<div id="pageContainer">
+  <div id="slideshow">
+    <div id="slidesContainer">
+      <div class="slide">
+       	<img src="1.jpg" />
+      </div>
+      <div class="slide">
+       	<img src="2.jpg" />
+      </div>
+      <div class="slide">
+       	<img src="3.jpg" />
+      </div>
+      <div class="slide">
+        <img src="4.jpg" />
+      </div>
+      <div class="slide">
+        <img src="5.jpg" />
+      </div>
+    
+    </div>
+  </div>
+</div>
+</body>
