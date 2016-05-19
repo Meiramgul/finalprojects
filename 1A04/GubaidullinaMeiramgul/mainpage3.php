fd25d83af4a6415aeefe886ed68520be112453c1
Meiramgul <meiramgul.gubaidullina@ce.sdu.edu.kz>
2016-05-19 09:55:59 +0600

    Create mainpage3.php

---
+++ 1A04/GubaidullinaMeiramgul/mainpage3.php
@@ -0,0 +1,43 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link type="text/css" rel="stylesheet" href="mainss.css" />
+<title>Online 107.2</title>
+</head>
+<body>
+<hr />
+<br />
+<div id="asd">
+<p id="firstp"><a href="logout.php">Log out</a></p>
+<img id="logo" src="logo.png" align="left"/> 
+<br/>
+
+<ul style="display:inline;">
+<li><a href="illnesses.php">Deseases</a></li>
+<li><a href="diet.php">Diet</a></li>
+<li><a href="procedure.php">Procedure</a></li>
+<li><a href="analyze.php">Analyzes</a></li>
+</ul>
+</div>
+<br/>
+<div id="firstd">
+<table id="firstt">
+	<tr> 
+		<td class="tds"><a id="drtest" href="mainpage2.php">Main page</a></td>
+		<td class="tds"><a id="drugs" href="drugs.php">Drugs</a></td>
+		<td class="tds"><a id="bio" href="bio.php">Bioadditive</a></td>
+		<td class="tds"><a id="mkup" href="avto.php">My account</a></td>
+	</tr>
+</table>
+    </div>
+<div id="gen">
+<p>Our online resource offers the fastest and most convenient way of dispensing medicine to our customers. The medicines themselves are readily available 24 x 7 with the help of the World Wide Web. We offer you not just the satisfaction and ease of not having to join long lines but also privacy. Because you don’t have to go to a doctor’s clinic for a check-up or to a physical pharmacy to get your prescription filled. So, nobody has to know what your ailment is. Of course, you can order your medicine at the time that is most convenient to you. Wouldn’t it be more pleasant if you can just order from your home or office, not having to beat the traffic? Don’t worry about ordering online using your credit cards. Our website has been secured to ensure that your personal and credit card details will remain private.</p>
+
+<h3>Online Ordering Concerns</h3>
+<p>Some costumers feel a little uneasy about ordering anything online. If you are of these people, rest assured that ordering online is just the same as ordering from a physical pharmacy. The only difference that you will experience is that you do not get to actually see us. The services, however, are just the same. The products are also just as reliable and genuine. That is something that you can hold onto.</p>
+
+<h3>Variety and Quality of Products</h3>
+<p>Not only are our products genuine but they are also wide in range. We are also continuously looking to acquire more products so that you will get the medicine that is best suited to your medical needs. At the moment, we are sure that we can already provide you with the products that you are seeking. You will be able to get the right products right on time. Yes, we also offer fast delivery as part of our efforts to constantly improve our services.</p>    
+</div>
+</body>
+</html>
