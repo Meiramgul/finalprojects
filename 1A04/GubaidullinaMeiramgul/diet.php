df6af0b4c220ea70816f8e1f0e3cfba7e88d6bbb
Meiramgul <meiramgul.gubaidullina@ce.sdu.edu.kz>
2016-05-19 09:46:20 +0600

    Update and rename cars_form2.php to diet.php

---
+++ 1A04/GubaidullinaMeiramgul/diet.php
@@ -0,0 +1,47 @@
+<?php
+include("mainpage.php");
+?>
+<html>
+<head>
+</head>
+<body>
+<table class="diet">
+<tr>
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">African Mango</a></td>    
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Instinct Diet</td>        
+</tr>
+<tr>
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Alkaline Diet</td>    
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Jenny Craig</td>        
+</tr> 
+<tr>
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Alli</td>    
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">The Kind Diet</td>        
+</tr> 
+<tr>
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Baby Food Diet</td>    
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Lemonade Diet</td>        
+</tr> 
+<tr>
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Belly Fat Cure</td>    
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Living Low-Carb</td>        
+</tr> 
+<tr>
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Cabbage Soup Diet</td>    
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Morning Banana Diet</td>        
+</tr> 
+<tr>
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Cheater's Diet</td>    
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">New Beverly Hills Diet</td>        
+</tr> 
+<tr>
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Detox Diets</td>    
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">02 Diet</td>        
+</tr> 
+<tr>
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Eat More, Weigh Less?</td>    
+<td><a href="http://www.webmd.com/diet/evaluate-latest-diets">Raspberry Ketones</td>        
+</tr> 
+</table>
+</body>
+</html>
