8cf99b4598fcfd03d975a24298ab8c40a36fb248
Meiramgul <meiramgul.gubaidullina@ce.sdu.edu.kz>
2016-05-19 09:55:07 +0600

    Create main.php

---
+++ 1A04/GubaidullinaMeiramgul/main.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+<body>
+
+<?php
+session_start();
+$email=$_SESSION['email'];
+if ($email)
+{
+echo "Welcome $email | <a href='logout.php'>Logout</a>";
+}
+else
+{
+echo "You are not logged in";
+}
+?>
+
+</body>
+
+</html>
