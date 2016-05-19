1bbd9bb251d0c26c9ca2fb20415a6c7232977cb4
Meiramgul <meiramgul.gubaidullina@ce.sdu.edu.kz>
2016-05-19 09:55:28 +0600

    Create mainpage.php

---
+++ 1A04/GubaidullinaMeiramgul/mainpage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link type="text/css" rel="stylesheet" href="mainss.css" />
+<title>Online 107.2</title>
+</head>
+<body>
+<hr />
+<br />
+<div id="asd">
+<p id="firstp"> <a id   ="login" href="avto.php">Log in </a>/<a id="register" href="registr.php"> Register</a> </p>
+<img id="logo" src="logo.png" align="left"/> 
+<br/>
+
+<ul style="display:inline;">
+<li><a href="illnesses.php">Diseases</a></li>
+<li><a href="diet.php">Diet</a></li>
+<li><a href="procedure.php">Procedure</a></li>
+<li><a href="analyze.php">Analyzes</a></li>
+</ul>
+<form method="get" action="" id="srch"> 
+<table cellpadding="0px" cellspacing="-35px" > 
+<tr> 
+<td class="srch">
+<input type="text" name="zoom_query" placeholder="Search" style="width:100px; border:0px solid; height:20px; padding:0px 3px;"> 
+</td>
+<td class="srch"> 
+<input type="submit" value="" style="border-style: none; background: url('searchbutton3.gif') no-repeat; width: 26px; height: 20px;">
+</td>
+</tr>
+</table>
+</form>
+</div>
+<br/>
+<div id="firstd">
+<table id="firstt">
+	<tr> 
+		<td class="tds"><a id="drtest" href="mainpage2.php">Main page</a></td>
+		<td class="tds"><a id="drugs" href="drugs.php">Drugs</a></td>
+		<td class="tds"><a id="bio" href="bio.php">Bioadditive</a></td>
+		<td class="tds"><a id="mkup" href="avto.php">My account</a></td>
+	</tr>
+</table>
+    </div>
+</body>
+</html>
